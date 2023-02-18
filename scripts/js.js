091569caef25f5ff0bb82c0560c2edbf8bce71ef
Laura Augusta <laurah.algusta@gmail.com>
2023-02-18 02:41:23 -0300

    new secrion.

--- scripts/js.js
+++ scripts/js.js
@@ -13,13 +13,20 @@ $(document).ready(function(){
     var img = $("#imagemain")[0];
     const imageMobile = ["../assets/images/studio-ghibli-7415572_960_720-MOBILE.jpg"];
     const imageDesc = ["../assets/images/studio-ghibli-7415572_960_720.jpg"];
+
+    var img2 = $("#img2")[0];
+    const imageMobile2 = ["../assets/images/img2-MOBILE.jpg"];
+    const imageDesc2 = ["../assets/images/img2.jpg"];
+
     $(window).resize(function(){
         var width = $(window).width();
         if(width <= 959){
             img.src = imageMobile;
+            img2.src = imageMobile2;
         }
         else{
             img.src = imageDesc;
+            img2.src = imageDesc2;
         }
     });
 })
