208c4d04954b446d759d09ff130a953edbc5f4ef
Laura Augusta <laurah.algusta@gmail.com>
2023-02-17 22:56:24 -0300

    updating mobile view.

--- scripts/js.js
+++ scripts/js.js
@@ -1,11 +1,5 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
-    // const images = ["../assets/images/photomania-16a4f2cfa28b5dccd3ca06968e887d5e.jpg", "../assets/images/studio-ghibli-7415572_960_720.jpg"]
-    // var img = $("#imagemain")[0];
-    // setInterval(function(){
-    //     let random = Math.floor(Math.random() * 2)
-    //     img.src = images[random]
-    // }, 8000)
     $(function (){
         $(document).scroll(function () {
             var $nav = $(".navbar-dark");
@@ -14,3 +8,19 @@ window.addEventListener('DOMContentLoaded', (event) => {
     });
 
 });
+
+$(document).ready(function(){
+    var img = $("#imagemain")[0];
+    const imageMobile = ["../assets/images/studio-ghibli-7415572_960_720-MOBILE.jpg"];
+    const imageDesc = ["../assets/images/studio-ghibli-7415572_960_720.jpg"];
+    $(window).resize(function(){
+        var width = $(window).width();
+        if(width <= 959){
+            img.src = imageMobile;
+        }
+        else{
+            img.src = imageDesc;
+        }
+    });
+})
+
